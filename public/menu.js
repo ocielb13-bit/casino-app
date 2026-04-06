7cba67812681cea70bb6f9cd3cae8b627a3bae40
ocielb13-bit <ocielb13@gmail.com>
2026-04-06 14:33:28 -0300

    Actualizar menu.js

--- public/menu.js
+++ public/menu.js
@@ -1,21 +1,27 @@
 async function login() {
   const username = document.getElementById("name").value;
-  const password = document.getElementById("password").value;
+  const password = document.getElementById("pass").value;
 
   const res = await fetch("/login", {
     method: "POST",
-    headers: {"Content-Type": "application/json"},
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password })
   });
 
   const data = await res.json();
 
-  console.log(data);
+  if (data.success) {
+    localStorage.setItem("user", username);
+    localStorage.setItem("saldo", data.balance);
 
-  if (!data.success) {
-    alert("Login incorrecto");
-    return;
+    // 🔥 REDIRECCIÓN SEGÚN ROL
+    if (data.role === "admin") {
+      window.location.href = "/admin.html";
+    } else {
+      window.location.href = "/menu.html";
+    }
+
+  } else {
+    document.getElementById("mensaje").innerText = "❌ Login incorrecto";
   }
-
-  alert("Login correcto 🎉");
 }
