35ea860b5083aae73964d8d2572bc2cf893efdcb
ocielb13-bit <ocielb13@gmail.com>
2026-04-06 14:48:04 -0300

    Actualizar server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import { createClient } from "@supabase/supabase-js";
 
 const app = express();
-app.use(express.json());
+app.use(express.static("public"));
 
 // 🔑 Supabase
 const supabase = createClient(
